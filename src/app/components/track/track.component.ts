94ab5a587b8c515ecafd02ce7beac38fd3eac668
Elad <elad@spectory.com>
2018-01-19 12:22:58 +0200

    adding play/pause adding mute functionallity

--- src/app/components/track/track.component.ts
+++ src/app/components/track/track.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit  } from '@angular/core';
 
 @Component({
   selector: 'app-track',
   templateUrl: './track.component.html',
   styleUrls: ['./track.component.scss']
 })
-export class TrackComponent implements OnInit {
-
+export class TrackComponent implements OnInit, AfterViewInit  {
+  player: any;
+  currentTrack: {
+    isPlaying: boolean;
+    isMuted: boolean;
+  };
   constructor() { }
 
   ngOnInit() {
+    this.currentTrack = { isPlaying: false,
+                          isMuted: false  };
+  }
+  ngAfterViewInit() {
+    this.player = document.getElementById('player');
+  }
+  togglePlay = () => {
+    this.currentTrack.isPlaying = !this.currentTrack.isPlaying;
+    if (this.currentTrack.isPlaying) {
+      this.player.play();
+    }
+    else {
+      this.player.pause();
+    }
+  }
+  toggleVolume = () => {
+    this.currentTrack.isMuted = !this.currentTrack.isMuted;
+    if (this.currentTrack.isMuted) {
+      this.player.muted = true;
+    }
+    else {
+      this.player.muted = false;
+    }
   }
 
 }
