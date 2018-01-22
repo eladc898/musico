f7be6cbb08604142451a3fc4d1a6d77333f5c03c
Elad <elad@spectory.com>
2018-01-22 12:06:20 +0200

    remove unnecessary import

--- src/app/data-service.ts
+++ src/app/data-service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-import 'rxjs/add/operator/toPromise';
-
 
 @Injectable()
 export class DataService {
   public apiHost = './assets/trackList.json';
   constructor(private http: Http) { }
 
+  // get data from json:
   public getAll(): Promise<Object> {
       return this.http.get(this.apiHost)
         .toPromise()
