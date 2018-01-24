b83db00bcdfe9fcdb6db7104169f92f4a6cd8b6f
Elad <elad@spectory.com>
2018-01-24 18:07:05 +0200

    WIP

--- src/app/components/track/track.component.spec.ts
+++ src/app/components/track/track.component.spec.ts
@@ -1,41 +1,72 @@
+import { Component, SimpleChanges } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import * as util from '@angular/core/src/change_detection/change_detection_util';
+import { FormsModule } from '@angular/forms';
 
 import { TrackComponent } from './track.component';
+import { SimpleChange } from '@angular/core/src/change_detection/change_detection_util';
+
+@Component({
+  template: ''
+})
+class TestHostComponent {
+  track: any;
+}
 
 fdescribe('TrackComponent', () => {
   const component: TrackComponent = new TrackComponent;
 
+  let fixture, track, testHost, element, de;
   beforeEach(() => {
-    component.ngOnInit();
-    component.track = {
+    TestBed.configureTestingModule({
+      declarations: [ TrackComponent, TestHostComponent ],
+      imports: [
+        FormsModule
+      ]
+    });
+    fixture = TestBed.createComponent(TrackComponent);
+    track = fixture.componentInstance;
+    testHost = fixture.componentInstance;
+    element = fixture.nativeElement;
+    de = fixture.debugElement;
+
+
+    track.ngOnInit();
+    track.track = {
       Id: 1,
       bpm: 120,
       owner: 'Ori Winokur',
       url: 'https://s3.amazonaws.com/candidate-task/Track+1.mp3'
     };
 
-    component.leaderTrack = {
+    track.leaderTrack = {
       Id: 3,
       bpm: 123,
       owner: 'Barak Inbar',
       url: 'https://s3.amazonaws.com/candidate-task/Track+2.mp3'
     };
-    component.isSync = false;
-    component.isPlayAll = false;
+    track.isSync = false;
+    track.isPlayAll = false;
   });
 
+
   it('should init component correctly', () => {
-    expect(component).toBeTruthy();
-    expect(component.player).toBeUndefined();
-    expect(component.progressBar).toBeUndefined();
+    expect(track).toBeTruthy();
+    expect(track.player).toBeUndefined();
+    expect(track.progressBar).toBeUndefined();
 
   });
 
   it('should init component view correctly', () => {
-    component.isPlayAll = true;
-    // component.ngOnChanges({});
-    expect(component.currentTrackInfo.isPlaying).toBeTruthy();
-    expect(component.showOriginalBpm).toBeFalsy();
-    
+    testHost.isPlayAll = true;
+    console.log('before ?', track);
+    // track.ngOnChanges({
+    //   isPlayAll: new SimpleChange(null, track.isPlayAll, true)
+    // });
+    fixture.detectChanges();
+    console.log('after ?', track);
+    // expect(track.isPlayAll).toBeTruthy();
+    // expect(track.player).toBeDefined();
+    // expect(track.progressBar).toBeDefined();
   });
 });
